847d331029486bca46c130a7efeca625f807a855
Greg McClement <greg.mcclement@gmail.com>
2020-10-30 08:10:43 -0700

    mising file

---
+++ demo/src/components/PaypalButtons.js
@@ -0,0 +1,116 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import scriptLoader from "react-async-script-loader";
+import Spinner from "./Spinner";
+
+const CLIENT = {
+  sandbox:"ATcFSR1buk8G26ZEiwWbHb9GIkLgpGNg8KRSEnN6zCGKBSgBdzlq0pdht5ZWIR7_hUhrIuFpKWEQRYha",
+  production:"your client id"
+};
+
+const CLIENT_ID =
+  process.env.NODE_ENV === "production" ? CLIENT.production : CLIENT.sandbox;
+
+let PayPalButton = null;
+class PaypalButton extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showButtons: false,
+      loading: true,
+      paid: false
+    };
+
+    window.React = React;
+    window.ReactDOM = ReactDOM;
+  }
+
+  componentDidMount() {
+    const { isScriptLoaded, isScriptLoadSucceed } = this.props;
+
+    if (isScriptLoaded && isScriptLoadSucceed) {
+      PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+      this.setState({ loading: false, showButtons: true });
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { isScriptLoaded, isScriptLoadSucceed } = nextProps;
+
+    const scriptJustLoaded =
+      !this.state.showButtons && !this.props.isScriptLoaded && isScriptLoaded;
+
+    if (scriptJustLoaded) {
+      if (isScriptLoadSucceed) {
+        PayPalButton = window.paypal.Buttons.driver("react", {
+          React,
+          ReactDOM
+        });
+        this.setState({ loading: false, showButtons: true });
+      }
+    }
+  }
+  createOrder = (data, actions) => {
+    console.log('data', data);
+    console.log('actions', actions);
+    return actions.order.create({
+      purchase_units: [
+        {
+          description: +"Entodicton Servers",
+          amount: {
+            currency_code: "USD",
+            value: 1
+          }
+        }
+      ]
+    });
+  };
+
+  onApprove = (data, actions) => {
+    print('data', data);
+    actions.order.capture().then(details => {
+      console.log('details', details);
+      const paymentData = {
+        payerID: data.payerID,
+        orderID: data.orderID
+      };
+      console.log("Payment Approved: ", paymentData);
+      this.setState({ showButtons: false, paid: true });
+    });
+  };
+
+  render() {
+    const { showButtons, loading, paid } = this.state;
+
+    return (
+      <div className="main">
+        {loading && <Spinner />}
+
+        {showButtons && (
+          <div>
+            <div>
+              <h2>Items: Mercedes G-Wagon</h2>
+              <h2>Total checkout Amount $200</h2>
+            </div>
+
+            <PayPalButton
+              createOrder={(data, actions) => this.createOrder(data, actions)}
+              onApprove={(data, actions) => this.onApprove(data, actions)}
+            />
+          </div>
+        )}
+
+        {paid && (
+          <div className="main">
+            <h2>
+            Subscription has been completed. The deployment id is SOMEGUID. You can monitor this page to see when the deployment is complete.
+            </h2>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default scriptLoader(`https://www.paypal.com/sdk/js?client-id=${CLIENT_ID}`)(PaypalButton);
