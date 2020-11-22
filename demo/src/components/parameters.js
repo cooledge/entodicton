c0d7d5e67aa41c8064e5e43e7cf2d36b1ca4ddf4
Greg McClement <greg.mcclement@gmail.com>
2020-11-22 00:18:57 +0000

    load balancer name changed

--- demo/src/components/parameters.js
+++ demo/src/components/parameters.js
@@ -3,7 +3,7 @@
 module.exports = 
 {
   "thinktelligence": {
-    "server": 'http://Think-Think-153KAK0JXKZI8-55668467.us-east-2.elb.amazonaws.com/api'
+    "server": 'http://Think-Think-1KUELMA2QK8PD-1771399070.us-east-2.elb.amazonaws.com/api'
     //"server": 'http://localhost:10000/api'
   }
 };
