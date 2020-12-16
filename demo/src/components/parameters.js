caf551bd778897cc75eb40af6a2f7ce4fa96d119
Greg McClement <greg.mcclement@gmail.com>
2020-12-15 17:18:15 -0800

    fix locations

--- demo/src/components/parameters.js
+++ demo/src/components/parameters.js
@@ -3,12 +3,12 @@
 module.exports = 
 {
   "thinktelligence": {
-    //"server": 'http://Think-Think-1KUELMA2QK8PD-1771399070.us-east-2.elb.amazonaws.com/api'
+    "server": 'http://Think-Think-1X3G67237CVP3-872441962.ca-central-1.elb.amazonaws.com/api'
     //"server": 'http://localhost:10000/api'
-    "server": 'http://184.67.27.82:5000'
+    //"server": 'http://184.67.27.82:5000'
   },
   "entodicton": {
-    "server": "Deplo-Entod-MFSSA8GZDW0Y-487383282.us-east-2.elb.amazonaws.com",
-    "apiKey": "c3b25efb-551e-4b59-8047-d2e6692ca645"
+    "server": "184.67.27.82",
+    "apiKey": "6804954f-e56d-471f-bbb8-08e3c54d9321"
   }
 };
