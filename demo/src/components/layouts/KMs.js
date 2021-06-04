04b04fb2c1c7ef4be45977b2084bf1490d273ea0
Greg McClement <greg.mcclement@gmail.com>
2021-06-04 14:17:50 -0700

    add demo link

--- demo/src/components/layouts/KMs.js
+++ demo/src/components/layouts/KMs.js
@@ -34,7 +34,7 @@ const KMs = () => {
     <div className='kms'>
       <h1 className='header'>Knowledge Modules</h1>
       <p>
-        A knowledge module is a predefined config file that can either be used as-is or by defining how new interface functions. The knowledge modules can be found in the <a href={"https://www.npmjs.com/package/ekms"} target="_blank">EKMS package</a>. Here is a list of existing KM's.
+        A knowledge module is a predefined config file that can either be used as-is or by defining how new interface functions. The knowledge modules can be found in the <a href={"https://www.npmjs.com/package/ekms"} target="_blank">EKMS package</a>. <a href={"https://youtu.be/WbrMExswiis"} target="_blank">Demo of knowledge module usage</a> Here is a list of existing KM's.
       </p>
       { listing }
     </div>
