4b6e0ef56747febcd9ecb8df74ba10e792706154
Greg McClement <greg.mcclement@gmail.com>
2021-09-01 20:38:08 -0700

    fixes

--- sr/src/App.js
+++ sr/src/App.js
@@ -4,15 +4,17 @@ import SRDemo from './srdemo';
 import parameters from './parameters'
 const { avatar } = require('ekms')
 
+const config = avatar;
+
 // chrome does not like me directly calling an http server from a page that loaded https. 
-//avatar.server('https://thinktelligence.com')
-avatar.server(parameters.thinktelligence.url)
+config.config.url = parameters.entodicton.url
+config.server(parameters.thinktelligence.url, parameters.entodicton.apiKey)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SRDemo km={avatar}></SRDemo>
+        <SRDemo km={config}></SRDemo>
       </header>
     </div>
   );
