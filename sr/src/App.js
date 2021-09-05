c8b6ab2a20fa03ef1f39e03c1497361446b74e57
Greg McClement <greg.mcclement@gmail.com>
2021-09-05 10:49:45 -0700

    have choices

--- sr/src/App.js
+++ sr/src/App.js
@@ -1,28 +1,53 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SRDemo from './srdemo';
 import parameters from './parameters'
-const { avatar, help, Config } = require('ekms')
+const { avatar, time, help, Config } = require('ekms')
 
-const config = avatar;
-config.add(help)
+const setupForDemo = (km) => {
+  const config = km;
+  config.add(help)
 
-// chrome does not like me directly calling an http server from a page that loaded https. 
-config.config.url = parameters.entodicton.url
-config.server(parameters.thinktelligence.url, parameters.entodicton.apiKey)
+  // chrome does not like me directly calling an http server from a page that loaded https. that is why.
+  // If you are using your own copy of entoditon make your own proxy server that will be configured with 
+  // your key and do this move.
 
-//console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", JSON.stringify(config.tests, null, 2))
-/*
-config.process('help').then( (result) => {
-  console.log('xxxxxxxxxxxxxxxxxxxxxxxx', result)
-})
-*/
+  // what to really call from the proxy server
+  config.config.url = parameters.entodicton.url
+  // the proxy server. I am passing the key but you should store the key on your server so 
+  // its not visible in the browser. Key argument is optional.
+  config.server(parameters.thinktelligence.url, parameters.entodicton.apiKey)
+  return config;
+}
+
+const configs = [
+  setupForDemo(avatar),
+  setupForDemo(time) 
+]
 
 function App() {
+  const [current, setCurrent] = useState(0);
+
+  const choose = (i) => {
+    setCurrent(i)
+  }
+
+  const choices = []
+  for (let i = 0; i < configs.length; ++i) {
+    choices.push(<button onClick={() => choose(i)}>{configs[i].name}</button>)
+    //choices.push(<button onClick={setCurrent(i)}>{configs[i].name}</button>)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <SRDemo km={config}></SRDemo>
+        <h2>
+          <list>
+            { choices }
+          </list>
+        </h2>
+        <SRDemo km={configs[current]}></SRDemo>
       </header>
     </div>
   );
