15116461701ad6087210e10026d77c429f2aadd0
Greg McClement <greg.mcclement@gmail.com>
2023-09-10 10:40:05 -0700

    remove old vide

--- demo/src/components/layouts/Videos.js
+++ demo/src/components/layouts/Videos.js
@@ -8,7 +8,6 @@ class Tutorial extends Component {
         <h1 className='header'>Developer Workflow Videos</h1>
         <a href={'https://youtu.be/b-fjpM238oQ'} target="_blank">Overview of the design and implementation</a>
         <a href={"https://youtu.be/4QDPLGjlq3g"} target="_blank">Demo of using knowledge modules from the EKMS package</a>
-        <a href={"https://www.youtube.com/watch?v=SRwJrvSVW7U"} target="_blank">Submit Bug Workflow</a>
         <a href={"https://www.youtube.com/watch?v=dR93bFJqCYA"} target="_blank">Debugging - Neural Nets Problems</a>
         <a href={"https://youtu.be/WbrMExswiis"} target="_blank">Demo of setting up a knowledge module</a>
         <br/>
