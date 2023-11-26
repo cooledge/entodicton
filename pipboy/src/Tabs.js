f2c76467d069a478ec438be1c77c11397206f2d8
Greg McClement <dev@thinktelligence.com>
2023-11-26 10:11:37 -0800

    fades - wip

--- pipboy/src/Tabs.js
+++ pipboy/src/Tabs.js
@@ -1,14 +1,29 @@
 function Tabs({ children, active, setActive }) {
-  const navLink = (name) => {
+  const navLink = (name, opacity) => {
     const role = name.toLowerCase() === active.toLowerCase() ? "tab" : ""
     return (
       <li className="nav-item">
-          <a className="nav-link" data-toggle="tab" role={role} onClick={() => setActive(name)}>{name.toUpperCase()}</a>                         
+          <a className="nav-link" style={{opacity}} data-toggle="tab" role={role} onClick={() => setActive(name)}>{name.toUpperCase()}</a>                         
       </li>
     )
   }
-  
-  const links = children.map( navLink )
+
+  const opacities = children.map( () => 0.2 )
+  const selected = children.findIndex( (name) => name === active )  
+  opacities[selected] = 1.0
+  if (opacities[selected+1]) {
+    opacities[selected+1] = 0.5
+  }
+  if (opacities[selected-1]) {
+    opacities[selected-1] = 0.5
+  }
+
+  // const links = children.map( navLink )
+  const links = []
+  for (let i = 0; i < children.length; ++i) {
+    links.push(navLink(children[i], opacities[i]))
+  }
+
   return (
     <ul className="nav nav-tabs">
       { links }
