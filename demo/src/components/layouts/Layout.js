916d86047fe3f256d559c425284fe85a36ad1d72
Greg McClement <dev@thinktelligence.com>
2024-01-06 11:18:52 -0800

    fix later

--- demo/src/components/layouts/Layout.js
+++ demo/src/components/layouts/Layout.js
@@ -28,7 +28,7 @@ class Layout extends Component {
                     <li className="barItem"><Link to='/tankdemo'>DEMO</Link></li>
                     { /* <li className="barItem"><Link to='/pipboy/'>PIPBOY DEMO</Link></li> */ }
                     <li className="barItem"><Link to='/pipboy/' target="_blank">PIPBOY</Link></li>
-                    <li className="barItem"><Link to='/tutorial'>TUTORIAL</Link></li>
+                    { /* <li className="barItem"><Link to='/tutorial'>TUTORIAL</Link></li> */ }
                     <li className="barItem"><Link to='/videos'>VIDEOS</Link></li>
                     <li className="barItem"><Link to='/kms'>KMS</Link></li>
                     { /* <li className="barItem"><Link to='/purchase'>PURCHASE</Link></li> */ }
