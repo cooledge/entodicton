e735f4851559c2fe968ce4ba0666a1d717c2398b
Greg McClement <dev@thinktelligence.com>
2024-06-16 20:20:58 -0700

    show the version on the about page

--- demo/src/components/layouts/About.js
+++ demo/src/components/layouts/About.js
@@ -2,13 +2,19 @@ import React, {Link, Component, useState} from 'react';
 import { useDispatch } from 'react-redux';
 import {connect} from 'react-redux';
 import { Form, Button } from 'react-bootstrap'
+const VERSION = require('./VERSION')
 const _ = require('underscore')
 
 class About extends Component {
   render(){
     return (
       <div className='about'>
-        Contact: <a href="mailto: dev@thinktelligence.com">email</a>
+        <div className='line'>
+          <span class='label'>Contact:</span> <span class='value'><a href="mailto: dev@thinktelligence.com">email</a></span>
+        </div>
+        <div className='line'>
+          <span class='label'>Version:</span> <span class='value'>{VERSION.version}</span>
+        </div>
       </div>
     )
   }
