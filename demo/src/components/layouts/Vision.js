b3ccb314d1e99ebef3f4c6e6dbd2ef53f49d0857
Greg McClement <greg.mcclement@gmail.com>
2024-07-18 20:45:03 -0700

    wip

---
+++ demo/src/components/layouts/Vision.js
@@ -0,0 +1,17 @@
+import React, { Component} from 'react';
+
+class Vision extends Component {
+    return (
+      <div className='vision'>
+        <div className='name'>The Programmable Mind</div>
+        <div className='details'>
+          <p>
+          You will wear a device that you can talk to. The device will be running the programmable mind. When you enter your Tesla it will talk through bluetooth to the tesla and download a knowledge module for the Tesla. You will be authenticated against the Tesla with the credentials on the device. Then you can talk to the Tesla and say turn on and the Tesla will start. You will tell the Tesla wear you want to go. While driving there you can ask for the air conditioning to be increased and how long to the destination. The knowledge module knows how to convert your commands into authorized commands that the Tesla performs. more details...
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Vision;
