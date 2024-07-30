26bc5920ec445e3628d9ddeb76c8bd35643e5b74
Greg McClement <greg.mcclement@gmail.com>
2024-07-30 08:33:41 -0700

    more details on reports

--- mongo/client/src/App.js
+++ mongo/client/src/App.js
@@ -4,9 +4,38 @@ import { useState } from 'react'
 import Query from './Query'
 import Report from './Report'
 
+const testData = () => {
+  const graph = {
+    type: "bar",
+    options: {
+      chart: {
+        id: 'apexchart-example'
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      }
+    },
+    series: [{
+      name: 'series-1',
+      data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+    }]
+  }
+
+  const tdata = {
+    headers: ['h1', 'h2', 'h2' ],
+    table: true,
+    rows: [
+      { columns: ['0A', '0B', { table: true, headers: [], rows: [{ columns: ['0Ca1', '0Ca2'] }, '0Cb', '0Cc'] }] },
+      { columns: ['1A', '1B', { graph }] },
+      { columns: ['2A', '2B', '2C'] },
+    ]
+  }
+
+  return tdata
+}
 
 function App() {
-  const [data, setData] = useState({})
+  const [data, setData] = useState(testData)
 
   const doQuery = (query) => {
     console.log("inDoquery")
--- mongo/client/src/Report.js
+++ mongo/client/src/Report.js
@@ -5,13 +5,29 @@ import Chart from 'react-apexcharts'
 const toHTML = (data) => {
   if (data.graph) {
     return fromGraph(data.graph)
-  } else if (data.rows) {
-    return fromRows(data.rows)
+  } else if (data.table) {
+    return fromTable(data)
   } else if (data.columns) {
     return fromCols(data.columns)
   } else {
     return data
   }
+}
+
+const fromTable = (table) => {
+    /*
+      <tr>
+        <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+      </tr>
+    */
+    const header = (headers) => {
+      if (headers && headers.length > 0) {
+        return <tr className='Header'>{ headers.map((h) => <th>{toHTML(h)}</th>) }</tr>
+      }
+    }
+    return <table className="Table">{header(table.headers)}{fromRows(table.rows)}</table>
 }
 
 const fromGraph = (graph) => {
@@ -21,7 +37,7 @@ const fromGraph = (graph) => {
 const fromRows = (data) => {
   const rows = []
   for (const row of data) {
-    const element = <div className='Row'>{ toHTML(row) }</div>
+    const element = <tr className='Row'>{ toHTML(row) }</tr>
     rows.push(element)
   }
   return rows
@@ -30,7 +46,7 @@ const fromRows = (data) => {
 const fromCols = (data) => {
   const cols = []
   for (const col of data) {
-    const element = <div className='Column'>{toHTML(col)}</div>
+    const element = <td className='Column'>{toHTML(col)}</td>
     cols.push(element)
   }
   return cols
@@ -39,62 +55,9 @@ const fromCols = (data) => {
 function Report(props) {
   const { data } = props
 
-  const tdata = [
-      ['0A', '0B', '0C'],
-      ['1A', '1B', '1C'],
-      ['2A', '2B', '2C']
-  ];
-
-  const graph = {
-    type: "bar",
-    options: {
-      chart: {
-        id: 'apexchart-example'
-      },
-      xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-      }
-    },
-    series: [{
-      name: 'series-1',
-      data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-    }]
-  }
-
-  const tdata2 = { 
-    rows: [
-      { columns: ['0A', '0B', { rows: [{ columns: ['0Ca1', '0Ca2'] }, '0Cb', '0Cc'] }] },
-      { columns: ['1A', '1B', { graph }] },
-      { columns: ['2A', '2B', '2C'] },
-    ]
-  }
-
-  const tt =  <table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Andersr</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-</table> 
-
   return (
     <div className="Report">
-      { tt }
-      <pre>{ table(tdata) }</pre>
-      <span>{JSON.stringify(data)}</span>
-      <div className="Table">
-        { toHTML(tdata2) }
-      </div>
+      { toHTML(data) }
     </div>
   );
 }
