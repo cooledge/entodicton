7c59f0cc3828a01328fff9263ffcc5e01fc7b2d3
Greg McClement <greg.mcclement@gmail.com>
2024-08-21 11:38:15 -0700

    more refactorign

--- mongo/server/data.js
+++ mongo/server/data.js
@@ -1,15 +1,30 @@
 const { MongoClient } = require('mongodb');
+const _ = require('lodash')
 
-/*
-  // for each item show the stores it sold in and total sales
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
 
-   {
-     collection: 'sample_supplies.sales',
-     group: 'items.name'
-     -> rows
-     list: {
-       properties: ['storeLocation']
-       total: :wq
-     }
-   }
-*/
+const instantiate = async (dataSpec) => {
+  const { dbName, collectionName, aggregation } = dataSpec
+  db = client.db(dbName);
+  collection = db.collection(collectionName)
+  let data;
+  if (_.isEmpty()) {
+    data = await collection.find().toArray();
+  } else {
+    data = await collection.aggregate(aggregation).toArray();
+  }
+  return data
+}
+
+const initialize = async () => {
+  await client.connect()
+  return client
+}
+
+module.exports = {
+  initialize,
+  instantiate,
+  client,
+}
+
--- mongo/server/query.js
+++ mongo/server/query.js
@@ -1,30 +1,19 @@
 const { MongoClient } = require('mongodb');
 const _ = require('lodash')
+const data = require('./data')
 const image = require('./image')
 
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
-
 const query = async (dataSpec, imageSpec) => {
-  const { dbName, collectionName, aggregation } = dataSpec
-  db = client.db(dbName);
-  collection = db.collection(collectionName)
-  let data;
-  if (_.isEmpty()) {
-    data = await collection.find().toArray();
-  } else {
-    data = await collection.aggregate(aggregation).toArray();
-  }
-  return image.instantiate(imageSpec, data)
+  return image.instantiate(imageSpec, await data.instantiate(dataSpec))
 }
 
 const initialize = async () => {
-  await client.connect()
-  return client
+  await data.initialize()
+  return data.client
 }
 
 module.exports = {
   initialize,
   query,
-  client,
+  client: data.client,
 }
