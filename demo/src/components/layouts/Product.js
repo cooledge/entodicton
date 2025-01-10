b87febe64a454e7718c7e4639d6fe0bdfdf7cf93
Greg McClement <greg.mcclement@gmail.com>
2025-01-10 10:15:00 -0800

    update details

--- demo/src/components/layouts/Product.js
+++ demo/src/components/layouts/Product.js
@@ -58,7 +58,12 @@ class Product extends Component {
           <p>
           ChatGPT is a neural net based solution that is trained on the internet. The Programmable Mind is a programmed solution. The use case of the Programmable Mind is as a UI for programs not as a chat based interaction. Such use cases would be controlling a report writer, interaction with computer in games (such as a Pipboy), language based UI for tradition business tools such as word processor or spreadsheets, ordering products by calling a API, logistics (for example "move box 1 to warehouse 5 bay 10"), language based UI for a webpages (knowledge module for webpage is loaded), language based UI for a location with devices (for example asking "where is the blender" in a kitchen), or controlling a robot ("go to the bathroom and clean the sink"). In other words, language based UI for an object or system that has internal state for which no training data exists. The Progammable Mind will do what it is programmed to do and nothing more. Knowledge modules allow multiple skills to be loaded at the same time. The Programmable Mind does not require tremendous computational resources or training.
           </p>
+          <p>
           Trainable systems based on neural nets store both their programs and data in a matrix of numbers that is effectively inscrutible. If a trainable system does not work as desired, modifying it through the normal development process is very difficult due to the program and data being stored in billions of numbers in a matrix rather than a data and a program written in an intelligible programming language. The Progammable Mind offers a traditional and effective intelligently written programming experience. Due to this the progammable mind uses significantly less resources that the neural net based solutions which require lots of memory and computational power (ie GPU's). The Programmable Mind can be used to produce a reliable systems worthy of trust and does not 'hallucinate' or 'lie'. 
+          </p>
+          <p>
+          This is a research project.
+          </p>
         </div>
       </div>
     )
