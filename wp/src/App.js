8f2e8c5321f8d3d720bd5f107ff918a7d851adb7
Greg McClement <greg.mcclement@gmail.com>
2025-02-05 08:30:37 -0800

    add query control

--- wp/src/App.js
+++ wp/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Text from './Text'
 import './css/wp.css'
+import Query from './Query'
 import { createEditor } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
 const tpmkms = require('tpmkms_4wp')
@@ -14,12 +15,17 @@ const initialValue = [
 ]
 
 const App = () => {
+  const [query, doQuery] = useState({ text: '', counter: 0 })
+  const [queryResponses, setQueryResponses] = useState([])
   const [editor] = useState(() => withReact(createEditor()))
   return (
     // Add the editable component inside the context.
-    <Slate editor={editor} initialValue={initialValue}>
-      <Editable />
-    </Slate>
+    <>
+      <Query doQuery={doQuery} queryResponses={queryResponses}/>
+      <Slate editor={editor} initialValue={initialValue}>
+        <Editable />
+      </Slate>
+    </>
   )
 }
 
---
+++ wp/src/Query.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+
+function Query(props) {
+  const { doQuery, queryResponses } = props
+  const [ query, setQuery ] = useState('')
+  const [ counter, setCounter] = useState(0)
+
+  useEffect( () => {
+    if (query.text === '') {
+      return
+    }
+    doQuery({ text: query, counter })
+  }, [query, doQuery, counter])
+
+  const onClick = () => {
+    const query = document.getElementById('query').value.toLowerCase()
+    document.getElementById('query').value = ''
+    setCounter(counter+1)
+    setQuery(query)
+  }
+
+  const keyPressed = (event) => {
+    if (event.key === 'Enter') {
+      onClick()
+    }
+  }
+
+  return (
+    <div className="Query">
+      <div>
+        <span className='label'>Request:</span> <input id='query' placeholder='press enter to submit.' autoFocus={true} onKeyDown ={ keyPressed } type='text' className='request' />
+        <a style={{"marginLeft": "10px"}} className="Button" id='submit' onClick={onClick}>Submit</a>
+      </div>
+      { query && 
+        <div>
+          <span className='paraphrase'><span className='label'>Paraphrase: </span><span className='field'>{ query }</span></span>
+        </div>
+      }
+      {
+        queryResponses && queryResponses.length > 0 &&
+        <span className='response'><span className='label'>Query Response:</span><span className='field'>{queryResponses}</span></span>
+      }
+    </div>
+  );
+}
+
+export default Query;
