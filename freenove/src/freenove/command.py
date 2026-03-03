084a21ab993bdbca94136aebe6f5156a3c27f2bf
Greg McClement <greg.mcclement@gmail.com>
2026-03-03 14:26:46 -0800

    add other modified file

---
+++ freenove/src/freenove/command.py
@@ -0,0 +1,13 @@
+class Command:
+    def __init__(self):
+        self.CMD_MOTOR = "CMD_MOTOR"
+        self.CMD_LED = "CMD_LED"
+        self.CMD_SERVO = "CMD_SERVO"
+        self.CMD_ACTION = "CMD_ACTION"
+        self.CMD_SONIC = "CMD_SONIC"
+        self.CMD_MODE ="CMD_MODE"
+        self.CMD_PAUSE ="CMD_PAUSE"
+        self.CMD_MULTI ="CMD_MULTI"
+        self.CMD_START_REPEATS ="CMD_START_REPEATS"
+        self.CMD_END_REPEATS ="CMD_END_REPEATS"
+
